275dccba42bd75a8bc2fed6bc7cd975edc3bdee6
Sergey Shipelevsky <sergey.shipelevsky@aternity.com>
2017-09-04 11:19:46 +0300

    WIP

--- gpstoggler3/obj/local/mips/objs/commander/Command.o.d
+++
@@ -1,8 +0,0 @@
-P:/Pr_SourceForge/GPSToggler3/gpstoggler3/obj/local/mips/objs/commander/Command.o: \
-  P:/Pr_SourceForge/GPSToggler3/gpstoggler3/jni/Command.cpp \
-  P:/Pr_SourceForge/GPSToggler3/gpstoggler3/jni/Command.h \
-  C:/Users/Sergey_s/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string
-
-P:/Pr_SourceForge/GPSToggler3/gpstoggler3/jni/Command.h:
-
-C:/Users/Sergey_s/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\string:
